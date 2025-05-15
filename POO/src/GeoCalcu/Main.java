1f3cfe2bdda4d07a7bea13564b26f32278ca5cb0
Davi Becker <davi_s_becker@estudante.sesisenai.org.br>
2025-05-15 20:27:15 -0300

    GeoCalculadoraAtual
    atualização do codigo

--- POO/src/GeoCalcu/Main.java
+++ POO/src/GeoCalcu/Main.java
@@ -4,30 +4,72 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int opc = 0;
-        System.out.println("Bem vindo ao Sistema de Calculador de Formas Geometricas");
-        System.out.println("qual forma você deseja usar?");
+        int opcao;
+        do {
+            System.out.println("Bem vindo ao Sistema de Calculador de Formas Geometricas");
+            System.out.println("qual forma você deseja usar?");
+            System.out.println(
+                    "Opções: \n" +
+                            "0. Sair\n" +
+                            "1. Quadrado\n" +
+                            "2. Retângulo\n");
+                opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 0:{
+                    System.out.println("Obrigado por usar o nosso sistema");
+                    break;
+                }
+                case 1: {
+                    System.out.println("Digite a cor do quadrado: ");
+                    String cor = sc.next();
+                    System.out.println("Digite o lado do quadrado: ");
+                    double lado = sc.nextDouble();
+
+                    Quadrado quadrado = new Quadrado(cor, lado);
+
+                    int opcao2;
+                    do {
+                        opcao2 = menuCalculo();
+
+                        if (opcao2 < 0 || opcao2 > 3) {
+                            System.out.println("Opção Inválida!");
+                        }
+
+                        switch (opcao2) {
+                            case 1: {
+                                System.out.println("Perimetro: " + quadrado.calculaPerimetro());
+                                break;
+                            }
+                            case 2: {
+                                System.out.println("Area: " + quadrado.calculaArea());
+                                break;
+                            }
+                            case 3: {
+                                System.out.println("Volume: " + quadrado.calculaVolume());
+                                break;
+                            }
+                        }
+                    } while (opcao2 != 0);
+
+                    System.out.println("Perimetro: " + quadrado.calculaPerimetro());
+                    System.out.println("Area: " + quadrado.calculaArea());
+                    System.out.println("Volume: " + quadrado.calculaVolume());
+                    break;
+                }
+            }
+        }while (opcao != 0);
+    }
+    static public int menuCalculo(){
+        System.out.println("qual calculo deseja fazer?");
         System.out.println(
                 "Opções: \n" +
-                "1. Quadrado\n" +
-                "2. Retângulo");
-        int opcao = sc.nextInt();
-
-        switch (opcao){
-            case 1: {
-                System.out.println("Digite a cor do quadrado: ");
-                System.out.println("Digite o lado do quadrado: ");
-                double lado = sc.nextDouble();
-
-                Quadrado quadrado = new Quadrado(cor, lado);
-
-                System.out.println("Perimetro: " + quadrado.calculaPerimetro());
-                System.out.println("Area: " + quadrado.calculaArea());
-                System.out.println("Volume: " + quadrado.calculaVolume());
-
-                break;
-            }
-        }
+                        "0. Voltar\n" +
+                        "1. Perimetro\n" +
+                        "2. Area\n" +
+                        "3. Volume\n");
+        return sc.nextInt();
     }
 }
